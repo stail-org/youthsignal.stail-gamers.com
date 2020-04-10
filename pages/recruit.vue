12bcea7a80ac0fdf227169ffa991e0f42ffca106
TigRig <tigrig.crex@gmail.com>
2020-04-10 18:24:29 +0900

    Add: Netlify forms, `RecruitForm` and `form.html`
    - `static/form.html` is dummy form to use Netlify forms on generated site

---
+++ pages/recruit.vue
@@ -0,0 +1,19 @@
+@ -0,0 +1,100 @@
+<template>
+  <main class="container">
+    <recruit-form />
+  </main>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+import RecruitForm from '~/components/parts/RecruitForm.vue'
+
+@Component({
+  components: {
+    RecruitForm,
+  },
+})
+export default class Recruit extends Vue {}
+</script>
