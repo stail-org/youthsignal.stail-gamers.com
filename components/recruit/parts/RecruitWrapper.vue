40d52cc19ee2190acfc38edc224ee2ddbf9e8cbf
TigRig <tigrig.crex@gmail.com>
2020-07-25 15:10:57 +0900

    Update: `recruit` page and add `RecruitWrapper`

---
+++ components/recruit/parts/RecruitWrapper.vue
@@ -0,0 +1,41 @@
+<template>
+  <main class="wrapper">
+    <div class="RecruitWrapper container px-md-5 py-4">
+      <slot></slot>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class RecruitWrapper extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  background: rgba(black, 0.3);
+  background-image: url('~assets/images/youthsignal-keyvisual.jpg');
+  background-attachment: fixed;
+  background-size: cover;
+  background-blend-mode: darken;
+}
+
+.RecruitWrapper {
+  max-width: $--md;
+  color: $color-lighter;
+  background: rgba($color-darker, 0.8);
+  &__Subtitle {
+    margin-top: 3rem;
+  }
+  &__Item {
+    padding-top: 0.5rem;
+  }
+  &__Overview {
+    &__Head {
+      font-size: 1.5rem;
+    }
+  }
+}
+</style>
---
+++ pages/recruit/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <recruit-wrapper>
+    <h1 class="text-center">募集一覧</h1>
+    <p>
+      <nuxt-link to="/recruit/voice-actor">
+        女性声優 兼 広報アシスタント
+      </nuxt-link>
+    </p>
+    <p><nuxt-link to="/recruit/scriptor">スクリプター</nuxt-link></p>
+  </recruit-wrapper>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+import RecruitWrapper from '~/components/recruit/parts/RecruitWrapper.vue'
+
+@Component({
+  components: {
+    RecruitWrapper,
+  },
+})
+export default class Recruit extends Vue {
+  head() {
+    return {
+      title: 'Recruit - YouthSignal',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            '同人サークル STail では、共に最高のゲームを作るメンバーを募集しています。',
+        },
+        {
+          hid: 'og:title',
+          name: 'og:title',
+          content: 'サークルメンバーを募集しています！【サークルSTail】',
+        },
+        {
+          hid: 'og:url',
+          name: 'og:url',
+          content: 'https://youthsignal.stail-gamers.com/recruit/',
+        },
+        {
+          hid: 'og:description',
+          name: 'og:description',
+          content: '共に、最高のゲームを作りませんか。',
+        },
+        {
+          hid: 'og:site_name',
+          name: 'og:site_name',
+          content: 'Recruit - YouthSignal',
+        },
+      ],
+    }
+  }
+}
+</script>
