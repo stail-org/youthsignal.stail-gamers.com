cd99263cac90039ce26b9e1489eabdc1e59457df
TigRig <tigrig.crex@gmail.com>
2021-01-04 17:20:18 +0900

    refactor: 右上リンクのコンポーネント構造変更

---
+++ components/index/parts/LinkBox.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="LinkBox">
+    <a class="white" :href="url" target="_blank" rel="noopener noreferrer">
+      {{ title }}
+      <fa class="fa" :icon="faExternalLinkAlt" />
+    </a>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
+
+@Component
+export default class LinkBox extends Vue {
+  @Prop()
+  title!: string
+
+  @Prop()
+  url!: string
+
+  get faExternalLinkAlt() {
+    return faExternalLinkAlt
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.LinkBox {
+  padding: 16px;
+  font-size: 0.8em;
+  background: rgba($--color-dark-cyan, 0.7);
+  border-radius: 8px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,12 @@
     <part-modal-container />
     <article class="Top__Article">
       <div class="Top__Article__Contents pb-5">
-        <part-trial-link />
+        <section-fixed-right-top>
+          <part-link-box
+            title="体験版はこちら！"
+            url="https://youthsignal-trial.stail-gamers.com/"
+          />
+        </section-fixed-right-top>
         <section-overview />
         <section-introduction />
         <section-story />
@@ -26,16 +31,17 @@ import SectionCharacter from '~/components/index/sections/Character.vue'
 import SectionImages from '~/components/index/sections/Images.vue'
 import SectionStaff from '~/components/index/sections/Staff.vue'
 import SectionSpec from '~/components/index/sections/Spec.vue'
+import SectionFixedRightTop from '~/components/index/sections/FixedRightTop.vue'
 
 import PartModalContainer from '~/components/index/parts/ModalContainer.vue'
-import PartTrialLink from '~/components/index/parts/TrialLink.vue'
+import PartLinkBox from '~/components/index/parts/LinkBox.vue'
 
 import { viewStore } from '~/store'
 
 @Component({
   components: {
     PartModalContainer,
-    PartTrialLink,
+    PartLinkBox,
     SectionOverview,
     SectionIntroduction,
     SectionStory,
@@ -43,6 +49,7 @@ import { viewStore } from '~/store'
     SectionImages,
     SectionStaff,
     SectionSpec,
+    SectionFixedRightTop,
   },
 })
 export default class Index extends Vue {
