f3afcec82121bd8dcc8c64391446ddddf033ccca
OHMORIYUSUKE <omoriyusuke@gmail.com>
2022-12-01 14:03:03 +0900

    wip

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:14
-
-COPY . /workspace
-
-WORKDIR /workspace
-
-RUN npm install
-
-EXPOSE 3000
--- components/common/sections/Footer.vue
+++ components/common/sections/Footer.vue
@@ -1,5 +1,6 @@
 <template>
   <footer class="Footer w-100 py-3 text-center">
+    このホームページに掲載されている一切の文書・図版・写真等を手段や形態を問わず複製、転載することを禁じます。<br />
     © 2019 Stail
   </footer>
 </template>
