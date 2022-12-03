cda2ea79e716d3c3e1043ef32daaae6f5b29d4ae
TigRig <tigrig.crex@gmail.com>
2022-12-03 13:55:49 +0000

    chore: nuxt ファイル群移動に伴う修正
    - linter の .gitignore 参照修正
    - vetur の project フォルダ指定

---
+++ vetur.config.js
@@ -0,0 +1,17 @@
+// 参考: https://vuejs.github.io/vetur/guide/setup.html#advanced
+
+/** @type {import('vls').VeturConfig} */
+module.exports = {
+  // **optional** default: `{}`
+  // override vscode settings
+  // Notice: It only affects the settings used by Vetur.
+  settings: {
+    "vetur.useWorkspaceDependencies": true,
+    "vetur.experimental.templateInterpolationService": true
+  },
+  // **optional** default: `[{ root: './' }]`
+  // support monorepos
+  projects: [
+    './src',
+  ],
+}
