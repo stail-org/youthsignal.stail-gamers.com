132045b7343dadda6013e1ff5292fa4c25f99851
TigRig <tigrig.crex@gmail.com>
2022-12-05 01:44:41 +0000

    refactor(footer): 注意文のクラス名変更

--- src/components/common/sections/Footer.vue
+++ src/components/common/sections/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="Footer w-100 py-3 text-center">
-    <div class="Warning">
+    <div class="Footer__Warning">
       このホームページに掲載されている一切の文書・図版・写真等を手段や形態を問わず無断で複製、転載することを禁じます。
     </div>
     <div>
@@ -19,9 +19,10 @@ export default class Footer extends Vue {}
 <style scoped lang="scss">
 .Footer {
   background: black;
-}
-.Warning {
-  margin: 0.1px 4%;
-  font-size: xx-small;
+
+  &__Warning {
+    margin: 0.1px 4%;
+    font-size: xx-small;
+  }
 }
 </style>
